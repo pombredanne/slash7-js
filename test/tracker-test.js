57dc4a8a42ffadcbe8da9cd7a47cebd26ad0ce37
Akihito Tsukamoto <tsuka05@gmail.com>
2013-07-10 14:54:16 +0900

    Complement dot for domain

--- test/tracker-test.js
+++ test/tracker-test.js
@@ -28,6 +28,12 @@
         equal($ns.Cookie.getDomain(), ".example.com");
     });
 
+    test("init() with domain", function () {
+        $ns.init(code, { domain: "example.com"});
+        ok($ns.tracker);
+        equal($ns.Cookie.getDomain(), ".example.com");
+    });
+
     test("track()", function () {
         expect(0);
         $ns.init(code);
