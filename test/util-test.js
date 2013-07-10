0cc70644425cfa26e1604397187221feec7761fb
Akihito Tsukamoto <tsuka05@gmail.com>
2013-07-10 14:37:46 +0900

    Escape RegExp meta characters

--- test/util-test.js
+++ test/util-test.js
@@ -7,4 +7,9 @@
         notStrictEqual(obj, dupped);
         deepEqual(obj, dupped);
     });
+
+    test('escapeRegExpMeta', function() {
+      var str = "$^\\/()|?+*[]{}.-";
+      equal($ns.util.escapeRegExp(str) , "\\$\\^\\\\\\/\\(\\)\\|\\?\\+\\*\\[\\]\\{\\}\\.\\-");
+    });
 }(slash7));
